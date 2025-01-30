d74b87bdb9409053c29fa2956504f245b027fd9d
Greg Tiu <tiugreg2@gmail.com>
2025-01-29 19:56:47 -0500

    Added Azure AD integration for Authentication and Authorization

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,43 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new() { Title = "LibraryInventory", Version = "v1" });
+        c.AddSecurityDefinition("OAuth2", new OpenApiSecurityScheme
+        {
+            Description = "OAuth2.0 - Authorization Flow",
+            Name = "Authorization",
+            Type = SecuritySchemeType.OAuth2,
+            Flows = new OpenApiOAuthFlows
+            {
+                AuthorizationCode = new OpenApiOAuthFlow
+                {
+                    AuthorizationUrl = new Uri(builder.Configuration["SwaggerAuth:AuthorizationUrl"] ?? string.Empty),
+                    TokenUrl = new Uri(builder.Configuration["SwaggerAuth:TokenUrl"] ?? string.Empty),
+                    Scopes = new Dictionary<string, string>
+                    {
+                        { builder.Configuration["SwaggerAuth:Scope"] ?? string.Empty, "Access API as User" }
+                    }
+                }
+            }
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "OAuth2"
+                    }
+                },
+                new[] { builder.Configuration["SwaggerAuth:Scope"] }
+            }
+        });
+    });
 
 var app = builder.Build();
 
@@ -19,7 +56,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.OAuthClientId(builder.Configuration["SwaggerAuth:ClientId"]);
+        c.OAuthUsePkce();
+        c.OAuthScopeSeparator(" ");
+    });
 }
 
 app.UseHttpsRedirection();
