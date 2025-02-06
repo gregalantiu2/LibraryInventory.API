431a672d588b9b8d9e6c4a639ce51416670f8c08
Greg Tiu <tiugreg2@gmail.com>
2025-02-05 23:10:26 -0500

    WIP

--- LibraryInventory.Service/MapperProfiles/ItemMapperProfile.cs
+++ LibraryInventory.Service/MapperProfiles/ItemMapperProfile.cs
@@ -36,7 +36,7 @@ namespace LibraryInventory.Service.MapperProfiles
             CreateMap<ItemRequest, ItemPolicy>()
                 .ForCtorParam("itemPolicyId", opt => opt.MapFrom(src => src.ItemPolicyId));
 
-            CreateMap<ItemType, ItemTypeEntity>().ReverseMap();
+            CreateMap<ItemType, ItemTypeEntity>();
 
             CreateMap<ItemFineOccurenceType, ItemPolicyEntity>()
                 .ForMember(dest => dest.ItemFineOccurenceTypeId, opt => opt.MapFrom(src => src.ItemFineOccurenceTypeId));
@@ -48,15 +48,41 @@ namespace LibraryInventory.Service.MapperProfiles
                 .ForMember(dest => dest.ItemPolicyId, opt => opt.MapFrom(src => src.ItemPolicyId))
                 .ForMember(dest => dest.ItemFineOccurenceTypeId, opt => opt.MapFrom(src => src.ItemFineOccurenceType.ItemFineOccurenceTypeId));
 
-            CreateMap<ItemFineOccurenceType, ItemFineOccurenceTypeEntity>().ReverseMap();
+            CreateMap<ItemFineOccurenceType, ItemFineOccurenceTypeEntity>();
 
-            CreateMap<ItemBorrowStatus, ItemBorrowStatusEntity>().ReverseMap();
+            CreateMap<ItemBorrowStatus, ItemBorrowStatusEntity>();
 
             CreateMap<Item, ItemEntity>()
                 .ForMember(dest => dest.ItemPolicyId, opt => opt.MapFrom(src => src.ItemPolicy.ItemPolicyId))
                 .ForMember(dest => dest.ItemTypeId, opt => opt.MapFrom(src => src.ItemType.ItemTypeId));
 
-            CreateMap<ItemEntity, Item>();
+            CreateMap<ItemEntity, Item>()
+                .ForCtorParam("itemTitle", opt => opt.MapFrom(src => src.ItemTitle))
+                .ForCtorParam("itemDescription", opt => opt.MapFrom(src => src.ItemDescription))
+                .ForCtorParam("itemType", opt => opt.MapFrom(src => src.ItemType))
+                .ForCtorParam("itemPolicy", opt => opt.MapFrom(src => src.ItemPolicy))
+                .ForCtorParam("itemLocation", opt => opt.MapFrom(src => src.ItemLocation))
+                .ForCtorParam("itemId", opt => opt.MapFrom(src => src.ItemId))
+                .ForMember(dest => dest.ItemPolicy, opt => opt.MapFrom(src => src.ItemPolicy));
+
+            CreateMap<ItemPolicyEntity, ItemPolicy>()
+                .ForCtorParam("itemPolicyId", opt => opt.MapFrom(src => src.ItemPolicyId))
+                .ForCtorParam("itemPolicyName", opt => opt.MapFrom(src => src.ItemPolicyName))
+                .ForCtorParam("allowedToCheckout", opt => opt.MapFrom(src => src.AllowedToCheckout))
+                .ForCtorParam("maxRenewalsAllowed", opt => opt.MapFrom(src => src.MaxRenewalsAllowed))
+                .ForCtorParam("checkoutDays", opt => opt.MapFrom(src => src.CheckoutDays))
+                .ForCtorParam("fineAmount", opt => opt.MapFrom(src => src.FineAmount))
+                .ForCtorParam("itemFineOccurenceType", opt => opt.MapFrom(src => src.ItemFineOccurenceType));
+
+            CreateMap<ItemFineOccurenceTypeEntity, ItemFineOccurenceType>()
+                .ForCtorParam("itemFineOccurenceTypeId", opt => opt.MapFrom(src => src.ItemFineOccurenceTypeId))
+                .ForMember(dest => dest.ItemFineOccurenceTypeDescription, opt => opt.MapFrom(src => src.ItemFineOccurenceTypeDescription));
+
+            CreateMap<ItemTypeEntity, ItemType>()
+                .ForCtorParam("itemTypeId", opt => opt.MapFrom(src => src.ItemTypeId))
+                .ForMember(dest => dest.ItemTypeName, opt => opt.MapFrom(src => src.ItemTypeName));
+
+
         }
     }
 }
